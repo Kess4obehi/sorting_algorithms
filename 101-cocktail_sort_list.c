0e9cd1d0471c0a7b0d04b7ff3045cdd8dec99681
Loisyy <nkeiruloisy@yahoo.com>
2024-04-30 11:55:17 -0700

    cocktail_sort update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -64,7 +64,7 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *tail, *shaker;
-	bool shoke_not_stirred = false;
+	bool shaken_not_stirred = false;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -72,16 +72,16 @@ void cocktail_sort_list(listint_t **list)
 	for (tail = *list; tail->next != NULL;)
 		tail = tail->next;
 
-	while (shoke_not_stirred == false)
+	while (shaken_not_stirred == false)
 	{
-		shoke_not_stirred = true;
+		shaken_not_stirred = true;
 		for (shaker = *list; shaker != tail; shaker = shaker->next)
 		{
 			if (shaker->n > shaker->next->n)
 			{
 				swap_node_ahead(list, &tail, &shaker);
 				print_list((const listint_t *)*list);
-				shoke_not_stirred = false;
+				shaken_not_stirred = false;
 			}
 		}
 		for (shaker = shaker->prev; shaker != *list;
